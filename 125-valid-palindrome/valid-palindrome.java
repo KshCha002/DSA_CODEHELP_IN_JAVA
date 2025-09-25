ecb787d180752211f83022bd182be80e81d847b8
KshCha002 <120982549+KshCha002@users.noreply.github.com>
2025-09-25 11:24:26 +0530

    Time: 2 ms (98.76%) | Memory: 42.8 MB (90.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder copyString= new StringBuilder();
-        
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)>='A'&& s.charAt(i)<='Z')
-            {
-                copyString.append((char)(s.charAt(i)+32));
-            }
-            if(s.charAt(i)>='0'&& s.charAt(i)<='9')
-            {
-                copyString.append((char)(s.charAt(i)));
-            }
-            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-            copyString.append((char)(s.charAt(i)));
-            }
-        }
-        System.out.println(copyString);
         int l=0;
-        int r=copyString.length()-1;
+        int r=s.length()-1;
         while(l<r)
         {
-            if(copyString.charAt(l)!=copyString.charAt(r))
+            char left=s.charAt(l);
+            char right=s.charAt(r);
+            if(!Character.isLetterOrDigit(left))
+            {
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(right))
+            {
+                r--;
+                continue;
+            }
+            if(Character.toLowerCase(right)!=Character.toLowerCase(left))
             {
                 return false;
             }
