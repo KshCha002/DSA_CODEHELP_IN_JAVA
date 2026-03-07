7264d0b02ce97d7b1b20554f9bd1016dcae2c842
KshCha002 <120982549+KshCha002@users.noreply.github.com>
2026-03-08 00:25:59 +0530

    Time: 6 ms (89.01%) | Memory: 46.5 MB (23.49%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -1,32 +1,5 @@
 class Solution {
-    // public int countWords(String[] words1, String[] words2) {
-    //     Map<String, Integer> map1 = new HashMap<>();
-    //     Map<String, Integer> map2 = new HashMap<>();
-    //     int n1 = words1.length;
-    //     int n2 = words2.length;
-    //     //System.out.println("n1="+n1 +",n2="+n2);
-    //     int count = 0;
-
-    //     for (int i = 0; i < n1; i++) {
-    //         map1.put(words1[i], (map1.getOrDefault(words1[i], 0)) + 1);
-    //     }
-    //     for (int i = 0; i < n2; i++) {
-    //         if(map1.containsKey(words2[i]) && map1.get(words2[i])<=1 )
-    //         {
-    //             map1.put(words2[i],(map1.getOrDefault(words2[i], 0)) - 1);
-    //         }
-    //     }
-
-
-    //    // System.out.println("map1="+map1.keySet().size() +",val2="+map2.keySet().size());
-
-    //      for (String key : map1.keySet()){
-    //         if(map1.get(key)==0)
-    //         {count++;}
-    //      }
-    //      return count;
-    // }
-     public int countWords(String[] words1, String[] words2) {
+    public int countWords(String[] words1, String[] words2) {
         Map<String, Integer> map1 = new HashMap<>();
         Map<String, Integer> map2 = new HashMap<>();
         int n1 = words1.length;
@@ -38,47 +11,74 @@ class Solution {
             map1.put(words1[i], (map1.getOrDefault(words1[i], 0)) + 1);
         }
         for (int i = 0; i < n2; i++) {
-            map2.put(words2[i], (map2.getOrDefault(words2[i], 0)) + 1);
+            if(map1.containsKey(words2[i]) && map1.get(words2[i])<=1 )
+            {
+                map1.put(words2[i],(map1.getOrDefault(words2[i], 0)) - 1);
+            }
         }
 
 
        // System.out.println("map1="+map1.keySet().size() +",val2="+map2.keySet().size());
 
-        if (map1.keySet().size() < map2.keySet().size()) {
-            for (String key : map1.keySet()) {
-                int val1 = map1.getOrDefault(key, 0);
-                int val2 = map2.getOrDefault(key, 0);
-                if (val1 ==1 && val2== 1)
-
-                {
-                  
-                  count++;
-                } else {
-                    
-                    continue;
-                }
-            }
-
-        } else {
-           
-            if (map1.keySet().size() >= map2.keySet().size())
-               {  
-                for (String key : map2.keySet()) 
-                    {
-                    int val1 = map1.getOrDefault(key, 0);
-                    int val2 = map2.getOrDefault(key, 0);
-                    //System.out.println("key="+key+",val1="+val1+",val2="+val2);
-                    if (val1 ==1 && val2== 1)
-                        {  
-                          count++;
-                        } else {
-                            
-                            continue;
-                        }
-                }}
-        }
-        return count;
-
+         for (String key : map1.keySet()){
+            if(map1.get(key)==0)
+            {count++;}
+         }
+         return count;
     }
+    //  public int countWords(String[] words1, String[] words2) {
+    //     Map<String, Integer> map1 = new HashMap<>();
+    //     Map<String, Integer> map2 = new HashMap<>();
+    //     int n1 = words1.length;
+    //     int n2 = words2.length;
+    //     //System.out.println("n1="+n1 +",n2="+n2);
+    //     int count = 0;
+
+    //     for (int i = 0; i < n1; i++) {
+    //         map1.put(words1[i], (map1.getOrDefault(words1[i], 0)) + 1);
+    //     }
+    //     for (int i = 0; i < n2; i++) {
+    //         map2.put(words2[i], (map2.getOrDefault(words2[i], 0)) + 1);
+    //     }
+
+
+    //    // System.out.println("map1="+map1.keySet().size() +",val2="+map2.keySet().size());
+
+    //     if (map1.keySet().size() < map2.keySet().size()) {
+    //         for (String key : map1.keySet()) {
+    //             int val1 = map1.getOrDefault(key, 0);
+    //             int val2 = map2.getOrDefault(key, 0);
+    //             if (val1 ==1 && val2== 1)
+
+    //             {
+                  
+    //               count++;
+    //             } else {
+                    
+    //                 continue;
+    //             }
+    //         }
+
+    //     } else {
+           
+    //         if (map1.keySet().size() >= map2.keySet().size())
+    //            {  
+    //             for (String key : map2.keySet()) 
+    //                 {
+    //                 int val1 = map1.getOrDefault(key, 0);
+    //                 int val2 = map2.getOrDefault(key, 0);
+    //                 //System.out.println("key="+key+",val1="+val1+",val2="+val2);
+    //                 if (val1 ==1 && val2== 1)
+    //                     {  
+    //                       count++;
+    //                     } else {
+                            
+    //                         continue;
+    //                     }
+    //             }}
+    //     }
+    //     return count;
+
+    // }
 
 }
