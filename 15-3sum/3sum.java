39d6fdc220e1b333da55aa4271435edf5e6a6626
KshCha002 <120982549+KshCha002@users.noreply.github.com>
2026-03-09 23:51:17 +0530

    Time: 2073 ms (5.01%) | Memory: 55.4 MB (98.31%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,62 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
-         Arrays.sort(nums); 
-    List<List<Integer>> l =new ArrayList<>();
-    //Set<List<Integer>> s =new HashSet<List<Integer>>();//check set in java
-    if(nums.length==3)
+        Set<List<Integer>> l= new HashSet<List<Integer>>();
+   //Set<Integer> s = new HashSet<Integer>();
+        for(int i=0;i<n;i++)
+        {
+             Set<Integer> s = new HashSet<Integer>();
+            int j=i+1;
+            while(j<n)
             {
-                if( (nums[0]+nums[1]+nums[2])==0)
-                {   
-                 List<Integer> l1 = new ArrayList<>(Arrays.asList(nums[0],nums[1],nums[2]));
-
-                    l.add(l1);
-                    return l;
-                }
-                else
+                int tar= -(nums[j]+nums[i]);
+                if(s.contains(tar))
                 {
-                   
-                   return l;
-                    }
-            }
-
-
-int i=0;
-for(;i<n;i++)
-{
-    if(i>0 && nums[i]==nums[i-1])
-    continue;
-    int j= i+1;
-    int k=n-1;
-    while(j<n && j<k)
-    {
-        int sum =nums[i]+nums[j]+nums[k];
-        if(sum ==0)
-        {
-            l.add(Arrays.asList(nums[i],nums[j],nums[k]));
-            //s.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                   List<Integer> arr= new ArrayList(Arrays.asList(nums[i],nums[j],tar));
+                    Collections.sort(arr);
+                    l.add(arr);
+                }
+            s.add(nums[j]);
             j++;
-            k--;
-            while(j<k && nums[j]==nums[j-1]){j++;}
-            while(j<k && nums[k]==nums[k+1]){k--;}
-
-        }
-        else if(sum>0)
-        {
-            k--;
-           // while(j<k && nums[k]==nums[k+1]){k--;}
-        }
-        else if(sum<0)
-        {
-        j++;
-        while(j<k && nums[j]==nums[j-1]){j++;}
+            }
         }
 
-    }
+        
+        return new ArrayList(l);
 
-}
-
-//List<List<Integer>> list = new ArrayList<>(s);
-return l;
     }
 }
