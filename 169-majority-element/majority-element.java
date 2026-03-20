4b4d304f1865f0082a80112b74cbaf1b8c192004
KshCha002 <120982549+KshCha002@users.noreply.github.com>
2026-03-20 21:39:56 +0530

    Time: 2 ms (70.69%) | Memory: 55.9 MB (28.33%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,34 @@
 class Solution {
+
+    //moore's voting algo
     public int majorityElement(int[] nums) {
-        int count =1;
-        
-        int maj =nums[0];
-        int n=nums.length;
-        for(int i=1;i<n;i++)
+        int maxCount = 0;
+        int count=1;
+        int maxElement = nums[0];
+        for (int i=1;i<nums.length;i++)
         {
-            if(count==0){
-                maj=nums[i];
+            int n= nums[i];
+            
+            if(count==0)
+            {
+                maxElement=n;
             }
-            count=count+(nums[i]==maj?1:-1);
+            count=count+(maxElement==n?1:-1); 
         }
-        return maj;
+        return maxElement;
+       
+        // int count =1;
+        
+        // int maj =nums[0];
+        // int n=nums.length;
+        // for(int i=1;i<n;i++)
+        // {
+        //     if(count==0){
+        //         maj=nums[i];
+        //     }
+        //     count=count+(nums[i]==maj?1:-1);
+        // }
+        // return maj;
+    
     }
 }
