138a563cf6055bd0ea0865539e0518606b957c35
KshCha002 <120982549+KshCha002@users.noreply.github.com>
2026-03-25 23:53:01 +0530

    Time: 1 ms (99.93%) | Memory: 42.7 MB (38.40%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,35 +1,30 @@
 class Solution {
     public int reverse(int x) {
-        if(x<Integer.MIN_VALUE || x>Integer.MAX_VALUE )
+        long ans=0;
+        if(x<Integer.MIN_VALUE || x>Integer.MAX_VALUE)
         {
             return 0;
-            }
+        }
 
-         boolean neg =false;
-     
-        int signedInt = x;
-        if(signedInt< 0)
+
+        boolean sign = (x<0)?true:false ;
+        if(sign){x=-x;}
+        while(x>0)
         {
-            neg =true;
-            signedInt= - signedInt;
-        } 
-        long ans =0;
+            int mod=x%10;
+            ans=(ans*10)+mod;
+            x=x/10;
+
+        }
         
-        //ans=ans*mod;
-        while(signedInt>0)
+       
+
+ if(sign){ans=-ans;}
+
+        if(ans<Integer.MIN_VALUE || ans>Integer.MAX_VALUE)
         {
-            int mod =signedInt%10;
-            ans= (ans*10)+mod;
-            signedInt =signedInt/10;
-            
+            return 0;
         }
-        if(neg)
-        {
-            ans=-ans;
-        }
-         if (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) {
-        return 0; // Return 0 if overflow occurs
-    }
         return (int)ans;
     }
 }
